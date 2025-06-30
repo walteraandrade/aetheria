79d6620267c0814303e486bcbf677c5336709dcc
walteraandrade <walteraandrade@gmail.com>
2025-06-30 07:49:42 -0300

    fix: ensure i18n is initialized before use

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,7 +3,9 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 register('en', () => import('./translations/en.json'));
 register('pt', () => import('./translations/pt.json'));
 
-export const i18nInit = init({
-    fallbackLocale: 'en',
-    initialLocale: getLocaleFromNavigator(),
-});
+export const i18nInit = (async () => {
+    await init({
+        fallbackLocale: 'en',
+        initialLocale: getLocaleFromNavigator(),
+    });
+})();
